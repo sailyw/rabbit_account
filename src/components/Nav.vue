cd0cd5edbc4235f6fe6630750f38baf1fe603f8e
sailyw <1152827086@qq.com>
2020-05-24 14:12:21 +0800

    Money页面布局 滚动

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/money">记账</router-link>|
     <router-link to="/labels">标签</router-link>|
     <router-link to="/statistics">统计</router-link>
@@ -12,5 +12,8 @@ import Vue from "vue";
 export default Vue.extend({});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.nav {
+  border: 1px solid red;
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,94 @@
 <template>
-  <div>
-    Money
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+    </div>
     <Nav />
   </div>
 </template>
@@ -13,5 +101,15 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
