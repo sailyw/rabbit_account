b0c75c3df0a607e8c206fd5c480b676e816dc175
sailyw <1152827086@qq.com>
2020-06-09 14:44:34 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,7 +22,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
+  background-color: rgb(150, 214, 247);
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
@@ -40,7 +42,7 @@ nav {
     }
   }
   > .item.selected {
-    color: red;
+    color:$color-highlight;
   }
 }
 </style>
