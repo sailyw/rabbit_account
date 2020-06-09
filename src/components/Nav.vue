a3eeadec5015a3b1c311b002658086547757c0c7
sailyw <1152827086@qq.com>
2020-06-09 11:43:13 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,46 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <Icon name="money"/>
-      记账
-    </router-link>|
-    <router-link to="/labels"><Icon name="labels"/>标签</router-link>|
-    <router-link to="/statistics"><Icon name="statistics"/>统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item">
+      <Icon name="money" />
+      记账 
+    </router-link>
+    <router-link to="/labels" class="item">
+      <Icon name="labels" />
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics" />
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-
 export default {
-  name: "Nav"
+  name: "Nav",
 };
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+nav{
+  display: flex;
+  box-shadow: 0 0 3px rgba(0,0,0,.25);
+  flex-direction: row;
+  font-size: 12px;
+  >.item{
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon{
+      width: 32px;
+      height: 32px;
+    }
+  }
+  >.item.selected{
+    color: red;
+  }
 }
 </style>
