e8bd11cbfe57c183f5d0fe7ce9527030f1b753d4
sailyw <yiwen.lu@bigita.com>
2021-07-08 15:51:10 +0800

    修改

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,13 +1,18 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link
+        class="tag"
+        v-for="tag in tags"
+        :key="tag.id"
+        :to="`/labels/edit/${tag.id}`"
+      >
         <span>{{ tag.name }}</span
         ><Icon name="right" />
-      </li>
-    </ol>
-    <div>
-      <button class="createTag" @click="createTag">新建标签</button>
+      </router-link>
+    </div>
+    <div class="createTag-wrapper">
+      <Button class="createTag" @click="createTag"> 新建标签 </Button>
     </div>
   </Layout>
 </template>
@@ -37,5 +42,34 @@ export default class Labels extends Vue {
 
 <style lang="scss" scoped>
 .tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  > .tag {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
 }
 </style>
