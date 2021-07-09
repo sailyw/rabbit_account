9c6b7ecd99dba61cb6921ecee7c30018b7b8016b
sailyw <yiwen.lu@bigita.com>
2021-07-09 17:56:56 +0800

    解决id生成问题

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,4 @@
+import createId from '@/lib/createId';
 const localStorageKeyName = 'tagList';
 type Tag = {
     id: string;
@@ -7,6 +8,8 @@ type TagListModel = {
     data: Tag[];
     fetch: () => Tag[];
     create: (name: string) => 'success' | 'duplicated';//联合类型
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+    remove: (id: string) => boolean;
     save: () => void;//void代表没用返回值
 }
 const tagListModel: TagListModel = {
@@ -25,6 +28,36 @@ const tagListModel: TagListModel = {
         this.data.push({ id: name, name: name });//由于id没有取数据库没法自增，只能取name
         this.save()
         return "success";
+    },
+    // 更新tag
+    update(id, name) {
+        const idList = this.data.map(item => item.id)
+        if (idList.indexOf(id) >= 0) {
+            const names = this.data.map(item => item.name);
+            if (names.indexOf(name) >= 0) {
+                return "duplicated"
+            } else {
+                const tag = this.data.filter(item => item.id === id)[0]
+                tag.name = name;
+                this.save()
+                return "success"
+            }
+        } else {
+            return 'not found'
+        }
+    },
+    //删除数据
+    remove(id: string) {
+        let index = -1;
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        this.data.splice(index, 1);
+        this.save();
+        return true;
     },
     //保存数据
     save() {
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,19 +1,20 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left" />
+      <Icon class="leftIcon" name="left" @click.native="goBack" />
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
       <FormItem
         :value="tag.name"
+        @update:value="update"
         field-name="标签名"
         placeholder="请输入标签名"
       />
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -40,6 +41,26 @@ export default class EditLabel extends Vue {
     } else {
       this.$router.replace("/404");
     }
+  }
+  update(name: string) {
+    console.log(name);
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+  remove() {
+    if (this.tag) {
+      if (tagListModel.remove(this.tag.id)) {
+        this.$router.back();
+      } else {
+        window.alert("删除失败");
+      }
+    }
+  }
+  goBack() {
+    console.log("返回");
+
+    this.$router.back();
   }
 }
 </script>
